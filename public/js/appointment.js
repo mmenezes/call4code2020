f535f7140d5ba2660cb5f189389bdc73501e0a23
Domnic Mendes <domnic.mendes@walgreens.com>
2020-07-04 17:25:45 +0530

    Added back button on inner pages Added appointment page which will trigger Chat window

---
+++ public/js/appointment.js
@@ -0,0 +1,11 @@
+$( document ).on( "pageinit", "#appointment", function( event ) {
+    //attach event handlers here. eg $('li').on('click', function(){});
+});
+
+$( document ).on( "pagebeforeshow", "#appointment", function( event ) {
+    //make ajax calls here
+});
+
+$( document ).on( "pageshow", "#appointment", function( event ) {
+    //make ui changes here eg show/hide dom elements
+});
